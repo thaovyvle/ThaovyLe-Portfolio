0a5cc2eb6fa8afdc4a07ea8f99b65b64918ec258
thaovyvle <122627050+thaovyvle@users.noreply.github.com>
2024-02-23 15:50:30 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@
 const nextConfig = {
   output: "export",
   basePath: '/ThaovyLePortfolio',
-  images: {
-    loader: "default",
-    path: "/_next/image",
-  },
 };
 
 module.exports = nextConfig;
