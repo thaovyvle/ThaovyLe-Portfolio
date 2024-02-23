0b0359ddcc34028610495248ab89976899fb7761
thaovyvle <122627050+thaovyvle@users.noreply.github.com>
2024-02-23 16:14:42 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: '/ThaovyLePortfolio',
+  basePath: '/ThaovyLe-Portfolio',
+  images: {
+    path: "/images",
+    loader: "default",
+  },
 };
 
 module.exports = nextConfig;
