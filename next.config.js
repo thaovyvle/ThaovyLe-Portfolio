bc1db3e75af47957ff943e87494e2073a1216633
thaovyvle <122627050+thaovyvle@users.noreply.github.com>
2024-02-23 10:57:57 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,15 +2,11 @@
 const nextConfig = {
   output: "export",
   basePath: '/ThaovyLe-Portfolio',
-}
-
-module.exports = {
-  env: {
-    RESEND_API_KEY: process.env.RESEND_API_KEY,
-  },
   experimental: {
     images: {
       unstable_styleRewriting: true,
     },
   },
 }
+
+module.exports = nextConfig
