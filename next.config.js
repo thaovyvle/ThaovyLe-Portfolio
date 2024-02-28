97c449c3cf334ec21fcf4eb441eecfb5ee84eb53
thaovyvle <122627050+thaovyvle@users.noreply.github.com>
2024-02-28 16:31:29 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: '/ThaovyLe-Portfolio',
+  assetPrefix: '/ThaovyLe-Portfolio,
+  env: {
+        BACKEND_URL: '/ThaovyLe-Portfolio',
+  },
 };
 
 module.exports = nextConfig;
