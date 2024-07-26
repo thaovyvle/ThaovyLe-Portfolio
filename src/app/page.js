8f580eae987b07b4236fa3c8b5077cf3698ce5b1
thaovyvle <122627050+thaovyvle@users.noreply.github.com>
2024-07-26 10:25:20 -0400

    Adding preloader

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,7 @@
 "use client";
+import { useEffect, useState } from 'react'
+import { AnimatePresence } from 'framer-motion';
+import Preloader from './components/Preloader';
 import Landing from "./components/Landing";
 import ParallaxScroll from "./components/ParallaxScroll";
 import Navbar from "./components/Navbar";
@@ -10,8 +13,29 @@ import RecentWork from './components/RecentWork';
 
 export default function Home() {
 
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect( () => {
+    (
+      async () => {
+          const LocomotiveScroll = (await import('locomotive-scroll')).default
+          const locomotiveScroll = new LocomotiveScroll();
+
+          setTimeout( () => {
+            setIsLoading(false);
+            document.body.style.cursor = 'default'
+            window.scrollTo(0,0);
+          }, 2000)
+      }
+    )()
+  }, [])
+ 
   return (
-    <main className="flex min-h-screen flex-col bg-[white]">
+    <main>
+      <AnimatePresence mode='wait'>
+        {isLoading && <Preloader />}
+      </AnimatePresence>
+    <div className="flex min-h-screen flex-col bg-[white]">
       <Navbar />
       <div className="container mt-20 mx-auto px-10">
         <Landing />
@@ -26,6 +50,7 @@ export default function Home() {
       </div>
       <SlidingImages />
       <Contact />
+      </div>
     </main>
   );
 }
